9cdbdd60a575d6b76f5347b7d72e76832cdd4c39
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-01-17 17:02:10 +0100

    update 1 défi Ultime

---
+++ Defi_Ultime/config.py
@@ -0,0 +1,11 @@
+import os
+SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
+
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FB_APP_ID = 3018046051780686
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = 1967148823570310
