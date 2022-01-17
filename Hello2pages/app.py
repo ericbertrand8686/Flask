c4a2c731dc97ba96442b083646fe11603b38ecd8
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-01-17 15:25:23 +0100

    Creation w gitignore venv

---
+++ Hello2pages/app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template("home.html", message_bienvenue="Bienvenue sur la page d'accueil !")
+
+@app.route("/next")
+def suite():
+    return render_template("page_suivante.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
