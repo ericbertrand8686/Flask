55fa00d8dd6abd2d3532cc36574592778ca59a02
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-01-26 22:31:58 +0100

    Valid_vote

--- Defi_Ultime/config.py
+++ Defi_Ultime/config.py
@@ -5,8 +5,8 @@ SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
 if os.environ.get('DATABASE_URL') is None:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    FB_APP_ID = 3018046051780686
+    FB_APP_ID = 625304428732787
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    FB_APP_ID = 3018046051780686
+    FB_APP_ID = 625304428732787
     #FB_APP_ID = 1967148823570310
